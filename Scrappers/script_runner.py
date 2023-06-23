de90c11c0e1cd9352758bd055a2b5c3e1411b375
ErraPrime <irimusrares7@gmail.com>
2023-06-23 14:38:32 +0300

    Primul commit

---
+++ Scrappers/script_runner.py
@@ -0,0 +1,34 @@
+
+import os
+import subprocess
+
+class Scraper:
+    def __init__(self, exclude):
+        """
+        Initialize the Scraper class.
+        """
+        self.exclude = exclude
+
+    def run(self):
+        """
+        Run the scraper for each Python file in the current directory, excluding the specified files.
+        """
+        path = os.path.dirname(os.path.abspath(__file__))
+        for site in os.listdir(path):
+            if site.endswith('.py') and site not in self.exclude:
+                try:
+                    subprocess.check_output(['python', os.path.join(path, site)], stderr=subprocess.STDOUT)
+                    print("Success scraping " + site)
+                except subprocess.CalledProcessError as e:
+                    print("Error in " + site)
+                    print(e.output.decode('utf-8'))
+
+if __name__ == "__main__":
+    # exclude files
+    exclude = ['__init__.py',
+               'setup_api.py',
+               'update_logo.py',
+                'TickleTech_Enterprise.py']
+
+    scraper = Scraper(exclude)
+    scraper.run()
