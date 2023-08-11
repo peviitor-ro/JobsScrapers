e3eba240eb6a589a8ec6f09a85781061b21dac07
Rarescode <irimusrares7@gmail.com>
2023-08-11 18:56:35 +0300

    Added bbcollection

---
+++ sites/bbcollection.py
@@ -0,0 +1,70 @@
+#
+#
+#
+# bbcollection > https://atp-group.ro/cariere/
+
+
+from sites.website_scraper_bs4 import BS4Scraper
+
+class bbcollectionScrapper(BS4Scraper):
+    
+    """
+    A class for scraping job data from bbcollection website.
+    """
+    
+    def __init__(self, company_name: str, url: str, company_logo_url: str):
+        """
+        Initialize the BS4Scraper class.
+        """
+        self.website_url = url
+        super().__init__(company_name, company_logo_url)
+        
+    def get_response(self):
+        self.get_content(self.website_url)
+    
+    def scrape_jobs(self):
+        """
+        Scrape job data from bbcollection website.
+        """
+
+        job_title_elements = self.get_jobs_elements('css_', "a > div > h5")
+        job_location_elements = self.get_jobs_elements('css_', "a > div > p:nth-child(2)")
+        job_url_elements = self.get_jobs_elements('css_', 'section:nth-child(7) > div > div > div > div > div > div > a')
+        
+        self.job_titles = self.get_jobs_details_text(job_title_elements)
+        self.job_urls = self.get_jobs_details_href(job_url_elements)
+        
+        job_cities = self.get_jobs_details_text(job_location_elements)
+        self.job_cities = []
+        
+        for job_city in job_cities:
+            self.job_cities.append(job_city.replace("Job disponibil in ", "").replace("Job disponibil in: ", "").split())
+    
+
+        self.format_data()
+        
+    def sent_to_future(self):
+        self.send_to_viitor()
+    
+    def return_data(self):
+        return self.formatted_data
+
+    def format_data(self):
+        """
+        Iterate over all job details and send to the create jobs dictionary.
+        """
+        for job_title, job_url, job_city in zip(self.job_titles, self.job_urls, self.job_cities):
+            job_url = f"https://www.bbcollection.ro{job_url}"
+            self.create_jobs_dict(job_title, job_url, "România", job_city)
+
+if __name__ == "__main__":
+    URL = 'https://www.bbcollection.ro/cariera.html'
+    URL_LOGO = 'https://www.bbcollection.ro/webroot/img/logo-bbcollection.jpg'
+    company_name = 'bbcollection'
+    bbcollection = bbcollectionScrapper(company_name, URL, URL_LOGO)
+    bbcollection.get_response()
+    bbcollection.scrape_jobs()
+    bbcollection.sent_to_future()
+    
+    
+
