da57a4e5d6c92f96f917ce9c05ec812814f5d1c2
Rarescode <irimusrares7@gmail.com>
2023-08-22 18:07:13 +0300

    Fixed job url being the same

--- sites/reinert.py
+++ sites/reinert.py
@@ -16,6 +16,7 @@ class reinertScrapper(BS4Scraper):
         Initialize the BS4Scraper class.
         """
         self.url = url
+        self.job_count = 1
         super().__init__(company_name, company_logo_url)
         
     def get_response(self):
@@ -43,7 +44,9 @@ class reinertScrapper(BS4Scraper):
         Iterate over all job details and send to the create jobs dictionary.
         """
         for job_title in self.job_titles:
-            self.create_jobs_dict(job_title, self.url, "România", "Oradea")
+            job_url = self.url + "#" + str(self.job_count)
+            self.create_jobs_dict(job_title, job_url, "România", "Oradea")
+            self.job_count += 1
 
 if __name__ == "__main__":
     URL = 'https://reinert-romania.ro/ro/cariere/'
