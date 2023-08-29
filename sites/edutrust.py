b552f59cd692cab98dc6c7bc11656fc40dd1dc22
Rarescode <irimusrares7@gmail.com>
2023-08-29 19:31:30 +0300

    Fixed edutrust job count

--- sites/edutrust.py
+++ sites/edutrust.py
@@ -16,6 +16,7 @@ class edutrustScrapper(BS4Scraper):
         Initialize the BS4Scraper class.
         """
         self.url = url
+        self.job_count = 1
         super().__init__(company_name, company_logo_url)
         
     def get_response(self):
@@ -43,7 +44,9 @@ class edutrustScrapper(BS4Scraper):
         Iterate over all job details and send to the create jobs dictionary.
         """
         for job_title in self.job_titles:
-            self.create_jobs_dict(job_title[3:], self.url, "Romania", "Oradea")
+            job_url = self.url + "#" + str(self.job_count)
+            self.create_jobs_dict(job_title[3:], job_url, "Romania", "Oradea")
+            self.job_count += 1
 
 if __name__ == "__main__":
     URL = 'https://www.edutrust.ro/cariera/#open-positions'
