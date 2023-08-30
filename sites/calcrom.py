453d59b7de74979c7944eadd58dc83784fd9daab
Rarescode <irimusrares7@gmail.com>
2023-08-30 19:47:32 +0300

    Fixed calrom jobs

--- sites/calcrom.py
+++ sites/calcrom.py
@@ -17,6 +17,7 @@ class calcromScrapper(BS4Scraper):
         Initialize the BS4Scraper class.
         """
         self.url = url
+        self.job_count = 1
         super().__init__(company_name, company_logo_url)
         
     def get_response(self):
@@ -45,7 +46,9 @@ class calcromScrapper(BS4Scraper):
         Iterate over all job details and send to the create jobs dictionary.
         """
         for job_title in self.job_titles:
-            self.create_jobs_dict(job_title, self.url, "Romania", "Iasi")
+            job_url = self.url + "#" + str(self.job_count)
+            self.create_jobs_dict(job_title, job_url, "România", "Iasi")
+            self.job_count += 1
 
 if __name__ == "__main__":
     URL = 'https://calcrom.ro/#careers'
