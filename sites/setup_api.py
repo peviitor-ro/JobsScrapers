850fc26dfd1cb7b1281981f31f71591e23dec33f
RaresCode <irimusrares7@gmail.com>
2024-09-23 19:53:13 +0300

    Update setup_api.py

--- sites/setup_api.py
+++ sites/setup_api.py
@@ -37,6 +37,7 @@ class UpdatePeviitorAPI:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
         }
 
+        print(requests.request("POST", "https://api.laurentiumarian.ro/get_token", headers=post_header, data=payload).json())
         self.access_token = requests.request("POST", "https://api.laurentiumarian.ro/get_token", headers=post_header, data=payload).json()['access']
         print(self.access_token)
 
