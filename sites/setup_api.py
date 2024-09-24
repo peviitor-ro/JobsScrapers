549637c8fab2d4f00bd92f7597b759cf4fd537f3
RaresCode <irimusrares7@gmail.com>
2024-09-24 18:48:05 +0300

    Update setup_api.py

--- sites/setup_api.py
+++ sites/setup_api.py
@@ -49,8 +49,8 @@ class UpdatePeviitorAPI:
         }
 
         print(f"Data List to send: {json.dumps(self.data_list)}")
-        requests.request("POST", "https://api.peviitor.ro/v5/add/", headers=post_header, data=json.dumps(self.data_list))
+        requests.request("POST", "https://api.laurentiumarian.ro/add", headers=post_header, data=json.dumps(self.data_list))
         print("ADD JOB REQUEST STATUS: ")
-        print(requests.request("POST", "https://api.peviitor.ro/v5/add/", headers=post_header, data=json.dumps(self.data_list)))
+        print(requests.request("POST", "https://api.laurentiumarian.ro/add", headers=post_header, data=json.dumps(self.data_list)))
         time.sleep(5)
 
