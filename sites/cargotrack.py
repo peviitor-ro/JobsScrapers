34b92b2ea5edda3aee877c20664b063444f87084
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-02-21 01:24:48 +0200

    Refactor cargotrackScraper to enhance job data scraping and URL handling

--- sites/cargotrack.py
+++ sites/cargotrack.py
@@ -29,10 +29,32 @@ class cargotrackScraper(BS4Scraper):
         Scrape job data from cargotrack website.
         """
 
-        job_titles_elements = self.get_jobs_elements('css_', "div > div.awsm-list-left-col > h2 > a")
+        job_cards = self.get_jobs_elements('css_', ".card")
         
-        self.job_titles = self.get_jobs_details_text(job_titles_elements)
-        self.job_urls = self.get_jobs_details_href(job_titles_elements)
+        self.job_titles = []
+        self.job_urls = []
+        
+        for card in job_cards:
+            card_text = card.get_text(separator=' | ', strip=True)
+            parts = card_text.split(' | ')
+            
+            if len(parts) >= 2:
+                title = parts[1].strip()
+                
+                links = card.select('a')
+                job_url = ''
+                for link in links:
+                    href = link.get('href', '')
+                    if href and href not in ['/vreau-sa-lucrez-la-cargotrack/', '/angajam-contabil-economist/']:
+                        if href.startswith('http'):
+                            job_url = href
+                        elif href.startswith('/'):
+                            job_url = 'https://cargotrack.ro' + href
+                        break
+                
+                if title and title != 'Vreau să lucrez la CargoTrack':
+                    self.job_titles.append(title)
+                    self.job_urls.append(job_url)
 
         self.format_data()
         
@@ -49,8 +71,6 @@ class cargotrackScraper(BS4Scraper):
         Iterate over all job details and send to the create jobs dictionary.
         """
         for job_title, job_url in zip(self.job_titles, self.job_urls):
-            if "Vreau să lucrez la CargoTrack!" in job_title:
-                continue
             self.create_jobs_dict(job_title, job_url, "România", "Oradea")
 
 if __name__ == "__main__":
@@ -58,6 +78,3 @@ if __name__ == "__main__":
     cargotrack.get_response()
     cargotrack.scrape_jobs()
     cargotrack.sent_to_future()
-    
-    
-
